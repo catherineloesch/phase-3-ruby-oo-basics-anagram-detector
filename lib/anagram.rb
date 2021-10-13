848e78963e6bd6c82f84f7aa2faaab0b8e7be71c
Catherine Loesch <catherine.loesch@protonmail.com>
2021-10-13 12:06:15 +0100

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_anagrams_arr)
+        possible_anagrams_arr.filter {|word| word.chars.sort == @word.chars.sort}
+    end
+end
